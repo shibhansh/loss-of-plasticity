b30c99c6e81f91471124f1442e70122cec015495
Jfernando4 <jfhernan@ualberta.ca>
2023-12-13 14:40:49 -0700

    update: added parameter initialization function

--- lop/nets/torchvision_modified_resnet.py
+++ lop/nets/torchvision_modified_resnet.py
@@ -238,3 +238,34 @@ class ResNet(nn.Module):
 
     def forward(self, x: Tensor, feature_list: list = None) -> Tensor:
         return self._forward_impl(x, feature_list)
+
+
+def build_resnet18(num_classes: int, norm_layer):
+    """
+    :param num_classes: number of classes for the classification problem
+    :param norm_layer: type of normalization. Options: [torch.nn.BatchNorm2d, torch.nn.Identity]
+    :return: an instance of ResNet with the correct number of layers for ResNet34
+    """
+    return ResNet(BasicBlock, layers=[2, 2, 2, 2], norm_layer=norm_layer, num_classes=num_classes)
+
+
+def kaiming_init_resnet_module(nn_module: torch.nn.Module):
+    """
+    Initializes the parameters of a resnet module in the following way:
+        - Conv2d: weights are initialized using xavier normal initialization and bias are initialized to zero
+        - Linear: same as Conv2d
+        - BatchNorm2d: bias are initialized to 0, weights are initialized to 1
+    :param nn_module: an instance ot torch.nn.Module to be initialized
+    """
+
+    if isinstance(nn_module, torch.nn.Conv2d) or isinstance(nn_module, torch.nn.Linear):
+        if isinstance(nn_module, torch.nn.Conv2d):
+            torch.nn.init.kaiming_normal_(nn_module.weight, nonlinearity="relu")
+        else:   # the only linear layer in a resnet is the output layer
+            torch.nn.init.kaiming_normal_(nn_module.weight, nonlinearity="linear")
+        if nn_module.bias is not None:
+            torch.nn.init.constant_(nn_module.bias, 0.0)
+
+    if isinstance(nn_module, torch.nn.BatchNorm2d):
+        torch.nn.init.constant_(nn_module.weight, 1.0)
+        torch.nn.init.constant_(nn_module.bias, 0.0)
